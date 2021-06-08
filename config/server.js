04fe0c8346602db5939fb4a9cf54f871af4a23c2
Neil Morrison <neil.morrison@A039-L018.local>
2021-06-07 20:10:48 -0400

    boilerplate

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21fe4b45f54e1981d2135bb7eb513c66'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0204e403-af98-4831-b681-692ddadadc60'
+};
